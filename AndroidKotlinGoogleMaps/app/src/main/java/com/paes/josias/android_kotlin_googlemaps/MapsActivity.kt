6c72debd48218f478c60eb8da527448fa2c0e7a2
Josias <josiaspaesjr@gmail.com>
2017-11-01 09:04:28 -0300

    Google Maps Example

--- AndroidKotlinGoogleMaps/app/src/main/java/com/paes/josias/android_kotlin_googlemaps/MapsActivity.kt
+++ AndroidKotlinGoogleMaps/app/src/main/java/com/paes/josias/android_kotlin_googlemaps/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.paes.josias.android_kotlin_googlemaps
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,10 +18,16 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var latlng: LatLng
+
+    lateinit var progress: ProgressDialog
+
+    private var strEnd = "";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +37,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
     }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapClickListener(this@MapsActivity)
 
         val jampa = LatLng(-7.1588384,-34.8576049)
 
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
         mMap.addMarker(createMarkers(jampa, "My Jampa", "A wonderfull city"))
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jampa, 17f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jampa, 18.5f))
     }
 
     fun createMarkers(latLng: LatLng, title:String, snippet:String): MarkerOptions{
@@ -38,6 +56,93 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .position(latLng)
                 .title(title)
                 .snippet(snippet)
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+                //.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+    }
+
+    override fun onMapClick(latlng:LatLng) {
+        this.latlng = latlng
+
+        progress = ProgressDialog(this)
+        progress.setTitle("Carregando endereço ...")
+        progress.show();
+
+        ReserveLatLngToAddress().execute()
+    }
+
+    fun goToAddress(v:View){
+
+        progress = ProgressDialog(this)
+        progress.setTitle("Carregando endereço ...")
+        progress.show();
+
+        strEnd = etEnd.text.toString()
+
+        ReserveAddressToLatLng().execute()
+    }
+
+
+    inner class ReserveLatLngToAddress : AsyncTask<Void, Void, Address>(){
+
+        override fun doInBackground(vararg p0: Void?): Address? {
+            try {
+
+                var geo = Geocoder(this@MapsActivity)
+                var addresses = geo.getFromLocation(this@MapsActivity.latlng.latitude
+                        ,this@MapsActivity.latlng.longitude
+                        , 1)
+
+                return addresses.get(0)
+
+            }catch (e:Exception){
+                return null
+            }
+        }
+
+        override fun onPostExecute(result: Address?) {
+
+            this@MapsActivity.progress.dismiss()
+
+            if(result != null){
+                this@MapsActivity.mMap.addMarker(
+                        this@MapsActivity.createMarkers(
+                                this@MapsActivity.latlng,
+                                result.thoroughfare,
+                                result.locality + " - " + result.postalCode
+                        )
+                )
+            }
+        }
+    }
+
+    inner class ReserveAddressToLatLng : AsyncTask<Void, Void, Address>(){
+
+        override fun doInBackground(vararg p0: Void?): Address? {
+            try {
+
+                var geo = Geocoder(this@MapsActivity)
+                val addresses = geo.getFromLocationName(this@MapsActivity.strEnd, 1)
+                return addresses.get(0)
+
+            }catch (e:Exception){
+                return null
+            }
+        }
+
+        override fun onPostExecute(result: Address?) {
+
+            this@MapsActivity.progress.dismiss()
+
+            if(result != null){
+                var latlng = LatLng(result.latitude, result.longitude)
+                this@MapsActivity.mMap.addMarker(
+                        this@MapsActivity.createMarkers(
+                                latlng,
+                                result.thoroughfare,
+                                result.locality + " - " + result.postalCode
+                        )
+                )
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 18.5f))
+            }
+        }
     }
 }
